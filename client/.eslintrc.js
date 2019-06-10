abc17b8bdfd80dabaf7d287e2c5aac6fed7a947b
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-10 12:57:54 +0100

    Navigate the user back to the home page when the button is clicked

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     "react/jsx-filename-extension": 0,
     "no-useless-constructor": 0,
     "react/jsx-one-expression-per-line": 0,
-    "no-console": 0
+    "no-console": 0,
+    "react/prefer-stateless-function": 0
   },
 };
